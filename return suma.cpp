e2c6d7f59d8ec2c49b78a1781dc71b4cafcfd7c7
wegyti <igor.wolak.j.a@gmail.com>
2025-10-25 14:21:25 +0200

    Create return suma.cpp

---
+++ return suma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
+
+int suma(int a, int b) {
+
+    int suma = a + b;
+    return suma;
+    
+}
+
+int main() {
+    cout << suma(5, 9);
+    cout << suma(2342423424, 3362685724235);
+    return 0;
+}
