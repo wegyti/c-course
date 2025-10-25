fede333c82c0f7d47a15fc42d4ef6c5bf268e0ba
wegyti <igor.wolak.j.a@gmail.com>
2025-10-25 15:57:29 +0200

    Create zad 9.cpp

---
+++ zad 9.cpp
@@ -0,0 +1,15 @@
+//9. Napisz funkcję minimum, która zwraca mniejszą z dwóch liczb.
+int minimum(int a, int b){
+	if (a < b) {
+		return b;
+
+	}
+	else {
+		return a;
+	}
+
+}
+int main() {
+	cout << minimum(2, 6);
+	return 0;
+}
