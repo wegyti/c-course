0f091e6f6d719e7dcc05f611cec7e30f043e8447
wegyti <igor.wolak.j.a@gmail.com>
2025-10-25 15:47:06 +0200

    Create zad 6.cpp

---
+++ zad 6.cpp
@@ -0,0 +1,11 @@
+//6. Napisz funkcję pole_prostokata, która zwraca pole prostokąta.
+int pole_prostokata(int a, int b) {
+	int pole_prostokata = a * b;
+	return pole_prostokata;
+
+
+}
+int main() {
+	cout << pole_prostokata(12, 7);
+		return 0;
+}
