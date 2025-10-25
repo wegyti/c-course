d0c31011eb1f11febcfea69e0778fb151d0d63ef
wegyti <igor.wolak.j.a@gmail.com>
2025-10-25 15:53:24 +0200

    Create zad 8.cpp

---
+++ zad 8.cpp
@@ -0,0 +1,9 @@
+//8. Napisz funkcję double obwod_kwadratu, która zwraca obwód kwadratu.
+double obwod_kwadratu(double a) {
+	double obwod_kwadratu = 4 * a;
+	return obwod_kwadratu;
+}
+int main() {
+	cout << obwod_kwadratu(4);
+	return 0;
+}
